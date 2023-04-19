9aa75213024a43cab41c38339c0e3898eed7838b
FelipeS0ares18 <felipeandresmessi10@gmail.com>
2023-04-18 23:33:34 -0300

    third commit

---
+++ script.js
@@ -0,0 +1,35 @@
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#botao");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#senha");
+
+let containerPassword = document.querySelector("#container-senha");
+
+let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!';
+let novaSenha = '';
+
+sizePassword.innerHTML = sliderElement.value;
+
+slider.oninput = function() {
+  sizePassword.innerHTML = this.value;
+}
+
+
+function gerarSenha(){
+
+  let pass = '';
+  for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+    pass += charset.charAt(Math.floor(Math.random() * n));
+  }
+  
+  console.log(pass)
+  containerPassword.classList.remove("hide");
+  password.innerHTML = pass;
+  novaSenha = pass;
+}
+
+function copyPassword(){
+  alert("Senha copiada com sucesso!")
+  navigator.clipboard.writeText(novaSenha);
+}

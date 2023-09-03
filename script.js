cd64a161ed4192afa2b792ec7598cf4d323148d8
Felipe Soares <131181873+FelipeS0ares18@users.noreply.github.com>
2023-09-03 02:00:07 -0300

    Fix: corrigindo funcionalidades do copypassword

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
 let sliderElement = document.querySelector("#slider");
 let buttonElement = document.querySelector("#botao");
-
 let sizePassword = document.querySelector("#valor");
 let password = document.querySelector("#senha");
-
 let containerPassword = document.querySelector("#container-senha");
-
 let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!';
 let novaSenha = '';
 
 sizePassword.innerHTML = sliderElement.value;
 
-slider.oninput = function() {
+sliderElement.oninput = function() {
   sizePassword.innerHTML = this.value;
 }
 
-
-function gerarSenha(){
-
+function gerarSenha() {
   let pass = '';
-  for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+  for (let i = 0, n = charset.length; i < sliderElement.value; ++i) {
     pass += charset.charAt(Math.floor(Math.random() * n));
   }
-  
+
   console.log(pass)
   containerPassword.classList.remove("hide");
   password.innerHTML = pass;
   novaSenha = pass;
 }
 
-function copyPassword(){
+function copyPassword() {
   alert("Senha copiada com sucesso!")
   navigator.clipboard.writeText(novaSenha);
 }
+
+
+let buttonCopiar = document.querySelector("#botao-copiar");
+buttonCopiar.addEventListener("click", copyPassword);
